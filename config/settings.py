e91237c1a05f4fbd068feb23e16c3ac877207f80
Bucky <buckyroberts@gmail.com>
2021-04-04 19:11:59 -0700

    Working on parsing logic

--- config/settings.py
+++ config/settings.py
@@ -5,3 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Data directories
 CSVS_DIR = os.path.join(BASE_DIR, 'csvs')
+
+REPOSITORIES = [
+    'Management'
+]
--- main.py
+++ main.py
@@ -20,13 +20,34 @@ def fetch(url):
     return results
 
 
+def get_payment_requests(issue):
+    """
+    Filter out the payment requests from a GitHub issue
+    """
+
+    lines = issue.get('body').splitlines()
+    payment_requests = [line for line in lines if line.startswith('[payment_request|') and line.endswith(']')]
+    return payment_requests
+
+
+def get_payment_request_data(issue):
+    """
+    Return payment request data
+    """
+
+    for payment_request in get_payment_requests(issue):
+        print(payment_request)
+
+
 def run():
     """
     Run main application
     """
 
-    data = fetch('https://api.github.com/repos/thenewboston-developers/Management/issues?labels=Payment+Due')
-    print(data)
+    issues = fetch('https://api.github.com/repos/thenewboston-developers/Management/issues?labels=Payment+Due')
+
+    for issue in issues:
+        get_payment_request_data(issue)
 
 
 if __name__ == '__main__':
