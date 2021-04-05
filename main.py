7c60d911fe0a1bee38d68f1d3163db0ff34f926a
Bucky <buckyroberts@gmail.com>
2021-04-04 18:57:13 -0700

    Authenticated requests

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
+import json
+from urllib.request import Request, urlopen
+
+from config.secret import GITHUB_OAUTH_TOKEN
+
+
+def fetch(url):
+    """
+    Fetch API data and return as Python object
+    """
+
+    request = Request(
+        url,
+        headers={
+            'Authorization': f'token {GITHUB_OAUTH_TOKEN}'
+        }
+    )
+    response = urlopen(request)
+    results = json.loads(response.read())
+    return results
+
+
 def run():
     """
     Run main application
     """
 
-    print(1)
+    data = fetch('https://api.github.com/repos/thenewboston-developers/Management/issues?labels=Payment+Due')
+    print(data)
 
 
 if __name__ == '__main__':
